5a3102665292641a15aa1ec1ed6b4ee74681f4e6
DESKTOP-NAIOPVF\Nagendra <tpuburu1966@atkinsglobal.com>
2024-08-12 05:11:04 +0530

    Hello world

---
+++ 01-intro/01-hello-world.go
@@ -0,0 +1,16 @@
+/*package decleration (mandatory)*/
+package main
+
+/* Importing the dependency packages*/
+import "fmt"
+
+/*package scoped variable/ constant /type decleration */
+/*package init function*/
+
+/* main function*/
+func main() {
+
+	fmt.Println("Hello World!!")
+}
+
+/*Other functions*/
